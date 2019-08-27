0605a573eb8d29ce65cfddeedb1099512c8bfe7d
Pásztor Bence <peace_96@hotmail.hu>
2019-08-27 10:00:32 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI:
-        'mongodb+srv://pisze:jelszo@vacationcalendar-phh9g.mongodb.net/calendar?retryWrites=true&w=majority',
-    secretOrKey:
-        'secret'
-};
